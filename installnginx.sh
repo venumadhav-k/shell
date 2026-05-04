0f6a0fbf4cf47f5cc86ea42e9f68c7577b55f21a
venumadhav-k <venumadhavkatta@hotmail.com>
2026-05-04 15:04:16 -0700

    commitforinstallationnginx

---
+++ installnginx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ];then
+    echo "Please run the script with root User access"
+    exit 1
+fi
+echo "intsalling Nginx" 
+apt-get update && apt-get install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "installing Nginx.....FAILURE"
+    exit
+else
+   echo "installing Nginx....SUCCESS"
+fi       
