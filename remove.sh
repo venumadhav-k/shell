78f6c375d3206359ff735b49483a0f7388016c7b
venumadhav-k <venumadhavkatta@hotmail.com>
2026-05-05 11:48:02 -0700

    codefixed

--- remove.sh
+++ remove.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+LOG_FILE="/var/logs/shell/out.txt"
+
 USER_ID=$(id -u)
 
 if [ "$USER_ID" -ne 0 ]; then
-  echo "Script must be executed with root user" | tee -a /var/logs/shell/out.txt
+  echo "Script must be executed with root user" | tee -a $LOG_FILE
   exit 1
 
   echo "Removing Packages"
   else
-  apt purge -y nginx &>> /var/logs/shell/out.txt
-  apt purge -y nodejs &>> /var/logs/shell/out.txt
-  apt autoremove -y &>> /var/logs/shell/out.txt
+  apt purge -y nginx &>> $LOG_FILE
+  apt purge -y nodejs &>> $LOG_FILE
+  apt autoremove -y &>> $LOG_FILE
 
   echo "Removed packages"
 
