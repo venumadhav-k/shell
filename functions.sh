dec950cfdd0970af9c864ec3cc5d60b3f9b881e8
venumadhav-k <venumadhavkatta@hotmail.com>
2026-05-05 12:56:41 -0700

    codefixed

--- functions.sh
+++ functions.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 USER_ID=$(id -u)
-LOG_FOLDER="/var/logs/shell"
-LOG_FILE="$LOG_FOLDER/$(basename $0).log"
-mkdir -p $LOG_FOLDER
+LOG_FOLDER="/var/log/shell"
+LOG_FILE="/var/log/shell/$(basename $0).log"
+
 
 if [ $USER_ID -ne 0 ]; then
   echo "Script must be executed with root user" | tee -a $LOG_FILE
   exit 1
 fi
- 
- VALIDATE() {
+ mkdir -p $LOG_FOLDER
+
+ VALIDATE(){
     if [ $1 -ne 0 ]; then
       echo "$2.....FAILURE" | tee -a $LOG_FILE
       exit 1
@@ -18,8 +19,7 @@ fi
     fi 
          
  }  
-  # $? :last command Exit status
-  # Install nginx
+    # Install nginx
   apt-get update &>> $LOG_FILE 
   apt-get install nginx -y &>> $LOG_FILE
   VALIDATE $? "Installing nginx"
--- remove.sh
+++ remove.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+LOG_FOLDER="/var/logs/shell/"
+LOG_FILE="/var/logs/shell/"
 
-LOG_FILE="/var/logs/shell/out.txt"
-
+mkdir -p $LOG_FOLDER
 USER_ID=$(id -u)
 
 if [ "$USER_ID" -ne 0 ]; then
   echo "Script must be executed with root user" | tee -a $LOG_FILE
   exit 1
+else
 
   echo "Removing Packages"
-  else
   apt purge -y nginx &>> $LOG_FILE
   apt purge -y nodejs &>> $LOG_FILE
   apt autoremove -y &>> $LOG_FILE
