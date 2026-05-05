938c64c7a8a4d76e2c8dd55747296d143be0acef
venumadhav-k <venumadhavkatta@hotmail.com>
2026-05-04 18:41:20 -0700

    codefixed

--- functions.sh
+++ functions.sh
@@ -2,15 +2,15 @@
 USER_ID=$(id -u)
 LOG_FOLDER="/var/logs/shell"
 LOG_FILE="/var/logs/shell/$0.log"
+mkdir -p $LOG_FOLDER
 
 if [ $USER_ID -ne 0 ]; then
   echo "Script must be executed with root user" | tee -a $LOG_FILE
   exit 1
 fi
- mkdir -p $LOG_FOLDER
-
+ 
  VALIDATE() {
-    if [ $1 -ne 0 ] ; then
+    if [ $1 -ne 0 ]; then
       echo "$2.....FAILURE" | tee -a $LOG_FILE
       exit 1
     else
@@ -19,7 +19,11 @@ fi
          
  }  
   # $? :Exit status
-  apt-get update && apt-get install mysql -y &>> $LOG_FILE
-    VALIDATE $? "Installing Mysql"  
- apt-get update && apt-get install nodejs -y &>> $LOG_FILE
-    VALIDATE $? "Installing nodejs"  
+  # Install MYSQL
+  apt-get update &>> $LOG_FILE 
+  apt-get install mysql-server -y &>> $LOG_FILE
+  VALIDATE $? "Installing Mysql"
+   # Install nodejs   
+ apt-get update &>> $LOG_FILE 
+ apt-get install nodejs -y &>> $LOG_FILE
+ VALIDATE $? "Installing nodejs"  
