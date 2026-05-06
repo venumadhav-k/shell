edc0dea6ff058a8c71d1cbe2269f2ad743fdf65e
venumadhav-k <venumadhavkatta@hotmail.com>
2026-05-06 16:20:42 -0700

    codefixed

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -3,8 +3,8 @@
 LOG_FOLDER=/home/venumadhavk33/app-log
 
 
-if [ !-d $LOG_FOLDER ]; then
-   echo "$LOG_FOLDER dosn't exist"
+if [ ! -d $LOG_FOLDER ]; then
+   echo -e "$LOG_FOLDER dosn't exist"
    exit 1
 fi
  FILES_TO_DELETE=$(find . -name "*.log" -type f -mtime +14)  
