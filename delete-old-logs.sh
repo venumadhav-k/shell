a1971d6841721cbc710a498c5689ac442c941bfa
venumadhav-k <venumadhavkatta@hotmail.com>
2026-05-06 16:26:48 -0700

    codefixed

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -8,4 +8,8 @@ if [ ! -d $LOG_FOLDER ]; then
    exit 1
 fi
  FILES_TO_DELETE=$(find $LOG_FOLDER -name "*.log" -type f -mtime +14)  
- echo "Files Name: $FILES_TO_DELETE" 
+ #echo "Files Name: $FILES_TO_DELETE" 
+
+ while IFS=read -r filepath; do
+ echo "deleting files : $filepath"
+ done <<< $FILES_TO_DELETE
