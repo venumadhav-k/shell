69587223c71b39e6abdc61491c73f93270246fb3
venumadhav-k <venumadhavkatta@hotmail.com>
2026-05-07 17:12:27 -0700

    codefixed

--- disk-usage.sh
+++ disk-usage.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 DISk_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=3
 
-whiel IFS= read -r line
+while IFS= read -r line
 do
    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
    PARTIATION=$(echo $line | awk '{print $7}')
