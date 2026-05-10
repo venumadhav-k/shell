0e66e8dfee4d2d28a91469c557699d9d6db8e2d5
venumadhav-k <venumadhavkatta@hotmail.com>
2026-05-09 23:45:56 -0700

    codechange

--- mail.sh
+++ mail.sh
@@ -6,7 +6,7 @@ ALERT_TYPE=$4
 #SERVER_IP-$5
 TO_TEAM=$5
 
-FINAL_MESSAGE=$(sed -e "s|/TO_TEAM/$TO_TEAM/|g" -e "s|/ALERT_TYPE/$ALERT_TYPE/|g" -e "s|/MESSAGE/$MESSAGE_BODY/|g" template.html)
+FINAL_MESSAGE=$(sed -e "s|TO_TEAM|$TO_TEAM|g" -e "s|ALERT_TYPE|$ALERT_TYPE|g" -e "s|MESSAGE|$MESSAGE_BODY|g" template.html)
 
 
 
